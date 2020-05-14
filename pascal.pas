46ab979ad35e5b91ae71dd6cb79cc084a1c5e1a2
Dmitry_S <dima270901@gmail.com>
2020-05-14 14:58:37 +0500

    Third version

--- pascal.pas
+++ pascal.pas
@@ -1,5 +1,5 @@
 ﻿program qq;
-var a,b,c,d:integer;
+var a,b,c,d,e:integer;
 begin 
 writeln('введите первое число');
 readln(a);
@@ -7,6 +7,8 @@ writeln('Введите второе число');
 readln(b);
 c:=a+b;
 d:=a*b;
+e:=(a*b)-(a+b);
 writeln('сумма = ',c);
 writeln('произведение = ',d);
+writeln('разность произведения и суммы = ',e);
 end.
